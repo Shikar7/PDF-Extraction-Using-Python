0725f75a3dd08860e156f88c7eb2b2fe64ace7fc
Shikar7 <134533611+Shikar7@users.noreply.github.com>
2024-08-25 17:24:01 +0530

    Removed un necessary files

--- table.py
+++
@@ -1,8 +0,0 @@
-import tabula 
-file = "iicai05.pdf"
-
-tables = tabula.read_pdf(file,pages='all',multiple_tables=True)
-print(len(tables))
-
-tabula.convert_into("iicai05.pdf", "File1.csv", output_format="csv", pages='all')
-
